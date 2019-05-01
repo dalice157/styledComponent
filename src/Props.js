bad0f07ac5fae1a3618894db691ab4a05f317cc6
DaChu <dalice.chu@104.com.tw>
2019-05-01 17:26:04 +0800

    刪除不要的註解

--- src/Props.js
+++ src/Props.js
@@ -2,7 +2,6 @@ import React, { Fragment } from 'react';
 import styled from 'styled-components';
 
 const Button = styled.button`
-  // 根据props是否用primary来设置颜色和背景颜色
     background: ${props => props.primary ? 'palevioletred' : 'white'};
     color: ${props => props.primary ? 'white' : 'palevioletred'};
     font-size: 1em;
